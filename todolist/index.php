ecf69197d38e386973eb8eeeb9d540ec60e6af3b
portable_ludo <rigoludo@gmail.com>
2018-11-21 15:53:30 +0100

    Ajout de delete mais ne marche que pour 1 suppression à la fois

--- todolist/index.php
+++ todolist/index.php
@@ -1,5 +1,14 @@
 <?php
+// Création d'un tableau
 $tasks = array();
+// Vérification que l'on reçoit une tâche à partir de la clé task du formulaire
+if(!empty($_POST["task"])) {
+    // Ajout d'un nouveau tableau dans notre tableau de tâches ($tasks)
+    $tasks[] = array(
+        "status" => false,
+        "task" => $_POST["task"]
+    );
+}
 $tasks[] = array(
     "status" => false,
     "task" => "Task 1"
@@ -9,42 +18,50 @@ $tasks[] = array(
     "task" => "Task 2"
 );
 $tasks[] = array(
-    "status" => false,
+    "status" => true,
     "task" => "Task 3"
 );
-?><!DOCTYPE html>
-<html lang="fr">
+if(isset($_GET["delete"])) {
+    $index = $_GET["delete"];
+    unset($tasks[$index]);
+}
+?><!doctype html>
+<html lang="en">
+<body>
+
+</body>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <form action="">
-        <input type="text" name="task" placeholder="Saisir une tâche..."/>
-        <button type="submit">OK</button>
-    </form>
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nom de la chaîne</th>
-                <th>Checker</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($tasks as $task): ?>
-            <tr>
-                <td><?php echo $task["status"] ? "-":"+" ?></td>
-                <td><?php echo $task["task"] ?></td>
-                <td><a href="#">check</a></td> <td><a href="#">modifier</a></td>
-                <td><a href="#">supprimer</a></td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
-</body>
+<!--<pre>-->
+<!--    --><?php //var_dump($tasks) ?>
+<!--</pre>-->
+<form action="" method="post">
+    <input type="text" name="task" placeholder="Saisir une tâche...">
+    <button type="submit">OK</button>
+</form>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Nom de la tâche</th>
+        <th>Checker</th>
+        <th>Modifier</th>
+        <th>Supprimer</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($tasks as $index => $task): ?>
+        <tr>
+            <!-- Ceci est presque comme une condition. C'est un ternaire -->
+            <td><?php echo $task["status"] ? "+" : "-" ?></td>
+            <td><?php echo $task["task"] ?></td>
+            <td><a href="#">check</a></td>
+            <td><a href="#">modifier</a></td>
+            <td><a href="?delete=<?php echo $index ?>">supprimer</a></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
 </html>
